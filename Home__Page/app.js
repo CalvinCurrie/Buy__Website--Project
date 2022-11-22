a7e424774e5d12637603cf3234115b0cc9764f46
Calvin <calvincurrie01@gmail.com>
2022-11-22 11:48:45 +1000

    FHF--Landing__Page--Finishing-UP__Draft

--- Home__Page/app.js
+++ Home__Page/app.js
@@ -1,3 +1,7 @@
-// onmousemove = function (e) {
-//   console.log("mouse location:", e.clientX, e.clientY);
-// };
+// let div = document.querySelector(".landing__page--description");
+
+// div.addEventListener("mousemove", function (e) {
+//   x = e.offsetX;
+//   y = e.offsetY;
+//   div.style.backgroundColor = `rgb(${x}, ${y}, ${x - y})`;
+// });
